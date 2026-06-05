7d8d1eb3ec3e762492982a858c1bb57267944002
wtf-lxc <130055900+wtf-lxc@users.noreply.github.com>
2026-06-05 15:10:19 +0800

    Add RadioGroup import to appstyle_setting_page.dart

--- simple_live_app/lib/modules/settings/appstyle_setting_page.dart
+++ simple_live_app/lib/modules/settings/appstyle_setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_live_app/widgets/radio_group.dart';
 import 'package:get/get.dart';
 import 'package:simple_live_app/app/app_style.dart';
 import 'package:simple_live_app/app/controller/app_settings_controller.dart';
