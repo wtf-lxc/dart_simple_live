47a25473cac42b904e9926ebc9f9f0c36a1c449b
wtf-lxc <130055900+wtf-lxc@users.noreply.github.com>
2026-06-05 15:19:39 +0800

    Fix: improve RadioGroup widget to pass groupValue

--- simple_live_app/lib/widgets/radio_group.dart
+++ simple_live_app/lib/widgets/radio_group.dart
@@ -1,8 +1,7 @@
 // Custom RadioGroup widget for simple_live_app
 import 'package:flutter/material.dart';
 
-/// A custom RadioGroup widget that wraps RadioListTile widgets
-/// and manages their group value state.
+/// A custom RadioGroup widget that manages radio button group state
 class RadioGroup<T> extends StatelessWidget {
   final T? groupValue;
   final ValueChanged<T?>? onChanged;
@@ -17,6 +16,52 @@ class RadioGroup<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return child;
+    return _RadioGroupProvider<T>(
+      groupValue: groupValue,
+      onChanged: onChanged,
+      child: child,
+    );
+  }
+}
+
+class _RadioGroupProvider<T> extends InheritedWidget {
+  final T? groupValue;
+  final ValueChanged<T?>? onChanged;
+
+  const _RadioGroupProvider({
+    required this.groupValue,
+    required this.onChanged,
+    required super.child,
+  });
+
+  static _RadioGroupProvider<T>? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<_RadioGroupProvider<T>>();
+  }
+
+  @override
+  bool updateShouldNotify(_RadioGroupProvider<T> old) {
+    return groupValue != old.groupValue || onChanged != old.onChanged;
+  }
+}
+
+/// Extension to help RadioListTile work with RadioGroup
+extension RadioGroupExtensions<T> on RadioListTile<T> {
+  RadioListTile<T> withGroupValue(T? groupValue, ValueChanged<T?>? onChanged) {
+    return RadioListTile<T>(
+      key: key,
+      value: value,
+      groupValue: groupValue,
+      onChanged: onChanged,
+      title: title,
+      subtitle: subtitle,
+      isThreeLine: isThreeLine,
+      dense: dense,
+      visualDensity: visualDensity,
+      shape: shape,
+      tileColor: tileColor,
+      selectedTileColor: selectedTileColor,
+      controlAffinity: controlAffinity,
+      autofocus: autofocus,
+    );
   }
 }
